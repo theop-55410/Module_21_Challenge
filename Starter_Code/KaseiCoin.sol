18f5a431aeba57139a2013407fa06d2003c677b3
Theo <theoprentice14@gmail.com>
2022-07-27 15:45:28 -0700

    initial commit

---
+++ Starter_Code/KaseiCoin.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+//  Import the following contracts from the OpenZeppelin library:
+//    * `ERC20`
+//    * `ERC20Detailed`
+//    * `ERC20Mintable`
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+
+// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor(
+        string memory coin_name,
+        string memory ticker,
+        uint supply
+        
+    )
+    ERC20Detailed(coin_name, ticker, 100)
+    public {
+
+    }
+}
